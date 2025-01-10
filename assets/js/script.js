513d9eb3ed956cfe737968a6e146de3be74e8e88
mmetrodw <artgure@gmail.com>
2025-01-10 16:24:14 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   assets/css/style.css
    	modified:   assets/js/script.js
    	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,17 @@
-const introDate = document.querySelector('.etl-timeline-opener-date');
-const date = '9 січня 2025';
-console.log(introDate);
-for(var i = 0; i < date.length; i++) {
-	const char = document.createElement('span');
-	char.innerHTML = date[i];
-	char.style.setProperty('--char-animation-delay', `${25 * i}ms`);
-	introDate.appendChild(char);
-}
-
 const eTimelineInitialSettings = {
 	target: null,
 	timeline: null,
 	options: {
 		theme: 'dark',
-		navigationPosition: 'bottom'
+		navigationPosition: 'bottom',
+		opener: {
+			enabled: true,
+			date: {
+				animationDuration: '1s',
+				animationIn: 'zoom-out',
+				animationOut: 'zoom-out'
+			}
+		}
 	}
 }
 
@@ -70,6 +68,11 @@ class eTimeline {
 			timelinePeriodBody.classList.add('etl-timeline-body');
 			timelinePeriod.appendChild(timelinePeriodBody);
 
+			const timelinePeriodDate = document.createElement('div');
+			timelinePeriodDate.classList.add('etl-timeline-date');
+			timelinePeriodDate.innerHTML = period.date;
+			timelinePeriodBody.appendChild(timelinePeriodDate);
+
 			if(period.heading) {
 				const timelinePeriodHeading = document.createElement('div');
 				timelinePeriodHeading.classList.add('etl-timeline-heading');
@@ -79,7 +82,7 @@ class eTimeline {
 
 			if(period.content) {
 				const timelinePeriodContent = document.createElement('div');
-				timelinePeriodContent.classList.add('etl-timeline-contenr');
+				timelinePeriodContent.classList.add('etl-timeline-content');
 				timelinePeriodContent.innerHTML = period.content;
 				timelinePeriodBody.appendChild(timelinePeriodContent);
 			}
@@ -93,11 +96,62 @@ class eTimeline {
 				timelinePeriodBody.appendChild(timelinePeriodLink);
 			}
 
+			if(this.options.opener.enabled) {
+				const timelinePeriodOpennerWrapper = document.createElement('div');
+				timelinePeriodOpennerWrapper.classList.add('etl-timeline-period-opener-wrapper');
+				timelinePeriodOpennerWrapper.style.setProperty('--animation-duration', this.options.opener.date.animationDuration);
+				timelinePeriod.appendChild(timelinePeriodOpennerWrapper);
+
+				const timelinePeriodOpennerDate = document.createElement('div');
+				timelinePeriodOpennerDate.classList.add('etl-timeline-period-opener-date');
+				timelinePeriodOpennerWrapper.appendChild(timelinePeriodOpennerDate);
+
+				for(var i = 0; i < period.date.length; i++) {
+					const dateChar = document.createElement('span');
+					dateChar.classList.add('etl-timeline-period-opener-date-char');
+					dateChar.innerHTML = period.date[i];
+					dateChar.style.setProperty('--char-animation-delay', `${i * 25}ms`);
+					dateChar.style.setProperty('--char-random-value', this.getRandomInteger(1, 10));
+					timelinePeriodOpennerDate.appendChild(dateChar);
+				}
+			}
 		});
+
+		this.uiElements.navigationItems = this.uiElements.navigationWrapper.querySelectorAll('.etl-navigation-item');
+		this.uiElements.timelinePeriods = this.uiElements.timelineWrapper.querySelectorAll('.etl-timeline-period');
+	}
+
+
+	addEventListeners() {
+		this.uiElements.navigationItems.forEach((item, index) => {
+			item.addEventListener('click', this.goToPeriod.bind(this, index))
+		});
+	}
+
+	goToPeriod(period) {
+		this.uiElements.navigationItems.forEach((item, index) => {
+			if(period === index) {
+				item.classList.add('current');
+			} else {
+				item.classList.remove('current');
+			}
+		});
+
+		this.uiElements.timelinePeriods.forEach((item, index) => {
+			if(index === period) {
+				this.removeAllClassesExcept(item, ['etl-timeline-period', 'current', `etl-opener-animation-in-${this.options.opener.date.animationIn}`]);
+			} else {
+				this.removeAllClassesExcept(item, ['etl-timeline-period']);
+			}
+		});
+
+		this.currentPeriod = period;
 	}
 
 	init() {
 		this.buildTimeline();
+		this.addEventListeners();
+		this.goToPeriod(0);
 	}
 
 	/* UTILS */
@@ -114,6 +168,15 @@ class eTimeline {
 	
 			return mergedObject;
 		}, {})
+	}
+
+	removeAllClassesExcept(element, classesToKeep = []) {
+		element.className = '';
+		classesToKeep.forEach(cls => element.classList.add(cls));
+	}
+
+	getRandomInteger(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 }
 
@@ -137,6 +200,11 @@ const DATA = [
 			text: 'Read more',
 			source: '#'
 		}
+	},
+	{
+		date: '7 january 2025',
+		heading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		content: 'Nam eros metus, molestie id ullamcorper et, auctor sed nunc. Proin semper non velit sit amet venenatis. Mauris egestas purus at erat imperdiet ullamcorper. In tincidunt hendrerit commodo. Nulla facilisi. Vestibulum convallis venenatis ex, non venenatis sem pulvinar ut. Quisque posuere aliquam enim ac eleifend. Cras efficitur tincidunt venenatis. Ut a malesuada odio. Nunc vel mi quis sapien auctor pretium eu ut mi. Fusce eget nisi congue, ultrices elit sed, semper libero. Integer dapibus orci in mauris tempor, non iaculis nulla fermentum. Nulla venenatis aliquam orci eget dapibus.',
 	}
 ];
 
@@ -145,6 +213,42 @@ new eTimeline({
 	timeline: DATA,
 	options: {
 		theme: 'dark',
-		navigationPosition: 'bottom'
+		navigationPosition: 'bottom',
+		opener: {
+			enabled: true,
+			date: {
+				animationDuration: '1s',
+				animationIn: 'zoom-out-letter-rotate',
+				animationOut: 'zoom-out'
+			}
+		}
 	}
 });
+
+
+
+const col = document.querySelectorAll('.col');
+
+col.forEach((item, index) => {
+	const timelinePeriodOpennerWrapper = document.createElement('div');
+	timelinePeriodOpennerWrapper.classList.add('etl-timeline-period-opener-wrapper');
+	timelinePeriodOpennerWrapper.style.setProperty('--animation-duration', '1s');
+	item.appendChild(timelinePeriodOpennerWrapper);
+
+	const timelinePeriodOpennerDate = document.createElement('div');
+	timelinePeriodOpennerDate.classList.add('etl-timeline-period-opener-date');
+	timelinePeriodOpennerWrapper.appendChild(timelinePeriodOpennerDate);
+
+	for(var i = 0; i < DATA[0].date.length; i++) {
+		const dateChar = document.createElement('span');
+		dateChar.classList.add('etl-timeline-period-opener-date-char');
+		dateChar.innerHTML = DATA[0].date[i];
+		dateChar.style.setProperty('--char-index', i);
+		dateChar.style.setProperty('--char-random-value', getRandomInteger(1, 10));
+		timelinePeriodOpennerDate.appendChild(dateChar);
+	}
+});
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
