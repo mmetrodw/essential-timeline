75db0ff1981cb2d9bce84b611ea266d9e814838a
mmetrodw <artgure@gmail.com>
2025-01-12 12:44:15 +0200

    modified:   assets/css/style.css 	new file:   assets/js/anime.min.js 	modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -227,28 +227,537 @@ new eTimeline({
 
 
 
-const col = document.querySelectorAll('.col');
+const timelinePeriodOpennerDate = document.querySelectorAll('.col .etl-timeline-period-opener-date');
 
-col.forEach((item, index) => {
-	const timelinePeriodOpennerWrapper = document.createElement('div');
-	timelinePeriodOpennerWrapper.classList.add('etl-timeline-period-opener-wrapper');
-	timelinePeriodOpennerWrapper.style.setProperty('--animation-duration', '1s');
-	item.appendChild(timelinePeriodOpennerWrapper);
+const prevData = '7 грудня 1943';
 
-	const timelinePeriodOpennerDate = document.createElement('div');
-	timelinePeriodOpennerDate.classList.add('etl-timeline-period-opener-date');
-	timelinePeriodOpennerWrapper.appendChild(timelinePeriodOpennerDate);
-
-	for(var i = 0; i < DATA[0].date.length; i++) {
+timelinePeriodOpennerDate.forEach((item) => {
+	for(var i = 0; i < prevData.length; i++) {
 		const dateChar = document.createElement('span');
 		dateChar.classList.add('etl-timeline-period-opener-date-char');
-		dateChar.innerHTML = DATA[0].date[i];
-		dateChar.style.setProperty('--char-index', i);
-		dateChar.style.setProperty('--char-random-value', getRandomInteger(1, 10));
-		timelinePeriodOpennerDate.appendChild(dateChar);
+		dateChar.setAttribute('value', 0);
+		dateChar.innerHTML = prevData[i];
+		item.appendChild(dateChar);
 	}
 });
 
-function getRandomInteger(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+const animationPresets = {
+	"in": {
+		"zoom-out": {
+			"translateZ": [500, 0],
+			"opacity": [0, 1],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-out-offset": {
+			"translateZ": [500, 0],
+			"opacity": [0, 1],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-out-offset-random": {
+			"translateZ": [() => { return anime.random(500, 1000) }, 0],
+			"opacity": [0, 1],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = anime.random(0, length) * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-out-letter-spacing": {
+			"parent": {
+				"scale": [2, 1],
+				"opacity": [0, 1],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				"loop": true,
+				"autoplay": true
+			},
+			"child" : {
+				"letterSpacing": ['25px', '0'],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				"loop": true,
+				"autoplay": true
+			}
+		},
+		"zoom-out-letter-fly-up": {
+			"parent": {
+				"scale": [2, 1],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				"loop": true,
+				"autoplay": true
+			},
+			"child": {
+				"translateY": ['100%', '0%'],
+				"easing": 'cubicBezier(.5, 0, 0, 1)',
+				'delay': (el, index, length) => {
+					const offset = length > 4 ? 25 : 100; 
+					const delay = index * offset;
+					return delay;
+				},
+				"loop": true,
+				"autoplay": true
+			}
+		},
+		"zoom-out-letter-fly-down": {
+			"parent": {
+				"scale": [2, 1],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				"loop": true,
+				"autoplay": true
+			},
+			"child": {
+				"translateY": ['-100%', '0%'],
+				"easing": 'cubicBezier(.5, 0, 0, 1)',
+				'delay': (el, index, length) => {
+					const offset = length > 4 ? 25 : 100; 
+					const delay = index * offset;
+					return delay;
+				},
+				"loop": true,
+				"autoplay": true
+			}
+		},
+		"zoom-out-letter-rotate": {
+			"parent": {
+				"scale": [2, 1],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				"loop": true,
+				"autoplay": true
+			},
+			"child": {
+				"rotateY": [90, 0],
+				"opacity": [0, 1],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				'delay': (el, index, length) => {
+					const offset = length > 4 ? 25 : 100; 
+					const delay = index * offset;
+					return delay;
+				},
+				"loop": true,
+				"autoplay": true
+			}
+		},
+		"letter-spacing": {
+			"opacity": [0, 1],
+			"letterSpacing": ['25px', '0'],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			"loop": true,
+			"autoplay": true
+		},
+		"letter-fly-up": {
+			"translateY": ['100%', '0%'],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"letter-fly-down": {
+			"translateY": ['-100%', '0%'],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"letter-rotate": {
+			"rotateY": [90, 0],
+			"opacity": [0, 1],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"letters-fade-out": {
+			"opacity": {
+				"value": [0, 1],
+				"duration": 100,
+				"delay": (el, index, length) => {
+					const offset = length > 4 ? 25 : 100;
+					const delay = index * offset;
+					return delay * 2;
+				},
+				"easing": 'linear'
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"letters-fade-out-random": {
+			"opacity": [0, 1],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = anime.random(0, length) * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-in": {
+			"translateZ": [-500, 0],
+			"opacity": [0, 1],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-in-offset": {
+			"translateZ": [-500, 0],
+			"opacity": [0, 1],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-in-offset-random": {
+			"translateZ": [() => { return anime.random(-500, -1000) }, 0],
+			"opacity": [0, 1],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = anime.random(0, length) * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+	},
+	"out": {
+		"zoom-out": {
+			"translateZ": [0, -500],
+			"opacity": [1, 0],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-out-offset": {
+			"translateZ": [0, -500],
+			"opacity": [1, 0],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-out-offset-random": {
+			"translateZ": [0, () => { return anime.random(-500, -1000) }],
+			"opacity": [1, 0],
+			"easing": 'cubicBezier(1, 0, 0., 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = anime.random(0, length) * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-out-letter-rotate": {
+			"parent": {
+				"translateZ": [0, -500],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				"loop": true,
+				"autoplay": true
+			},
+			"child": {
+				"rotateY": [0, 90],
+				"opacity": [1, 0],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				'delay': (el, index, length) => {
+					const offset = length > 4 ? 25 : 100; 
+					const delay = index * offset;
+					return delay;
+				},
+				"loop": true,
+				"autoplay": true
+			}
+		},
+		"letter-spacing": {
+			"opacity": [1, 0],
+			"letterSpacing": ['0px', '25px'],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			"loop": true,
+			"autoplay": true
+		},
+		"letter-fly-up": {
+			"translateY": ['0%', '-100%'],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"letter-fly-down": {
+			"translateY": ['0', '100%'],
+			"easing": 'cubicBezier(.5, 0, 0, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"letter-rotate": {
+			"rotateY": [0, 90],
+			"opacity": [1, 0],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"letters-fade-out": {
+			"opacity": {
+				"value": [1, 0],
+				"duration": 100,
+				"delay": (el, index, length) => {
+					const offset = length > 4 ? 25 : 100;
+					const delay = index * offset;
+					return delay * 2;
+				},
+				"easing": 'linear'
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"letters-fade-out-random": {
+			"opacity": [1, 0],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = anime.random(0, length) * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-in": {
+			"translateZ": [0, 500],
+			"opacity": [1, 0],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-in-offset": {
+			"translateZ": [0, 500],
+			"opacity": [1, 0],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = index * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-in-offset-random": {
+			"translateZ": [0, () => { return anime.random(500, 1000) }],
+			"opacity": [1, 0],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			'delay': (el, index, length) => {
+				const offset = length > 4 ? 25 : 100; 
+				const delay = anime.random(0, length) * offset;
+				return delay;
+			},
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-in-letter-spacing": {
+			"translateZ": [0, 500],
+			"opacity": [1, 0],
+			"letterSpacing": ['0px', '50px'],
+			"easing": 'cubicBezier(1, 0, 0.5, 1)',
+			"loop": true,
+			"autoplay": true
+		},
+		"zoom-in-letter-fly-up": {
+			"parent": {
+				"scale": [1, 2],
+				"easing": 'cubicBezier(.5, 0, 0, 1)',
+				"loop": true,
+				"autoplay": true
+			},
+			"child": {
+				"translateY": ['0%', '-100%'],
+				"easing": 'cubicBezier(.5, 0, 0, 1)',
+				'delay': (el, index, length) => {
+					const offset = length > 4 ? 25 : 100; 
+					const delay = index * offset;
+					return delay;
+				},
+				"loop": true,
+				"autoplay": true
+			}
+		},
+		"zoom-in-letter-fly-down": {
+			"parent": {
+				"scale": [1, 2],
+				"easing": 'cubicBezier(.5, 0, 0, 1)',
+				"loop": true,
+				"autoplay": true
+			},
+			"child": {
+				"translateY": ['0%', '100%'],
+				"easing": 'cubicBezier(.5, 0, 0, 1)',
+				'delay': (el, index, length) => {
+					const offset = length > 4 ? 25 : 100; 
+					const delay = index * offset;
+					return delay;
+				},
+				"loop": true,
+				"autoplay": true
+			}
+		},
+		"zoom-in-letter-rotate": {
+			"parent": {
+				"translateZ": [0, 500],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				"loop": true,
+				"autoplay": true
+			},
+			"child": {
+				"rotateY": [0, 90],
+				"opacity": [1, 0],
+				"easing": 'cubicBezier(1, 0, 0.5, 1)',
+				'delay': (el, index, length) => {
+					const offset = length > 4 ? 25 : 100; 
+					const delay = index * offset;
+					return delay;
+				},
+				"loop": true,
+				"autoplay": true
+			}
+		}
+	}
 }
+
+
+timelinePeriodOpennerDate.forEach((item) => {
+	const animationIn = item.dataset.animationIn;
+	const animationInDuration = parseInt(item.dataset.animationInDuration);
+	const animationOut = item.dataset.animationOut;
+	const animationOutDuration = parseInt(item.dataset.animationOutDuration);
+	const target = item.querySelectorAll('.etl-timeline-period-opener-date-char');
+
+	if(animationIn) {
+		switch(animationIn) {
+			case 'zoom-out-letter-fly-up':
+			case 'zoom-out-letter-fly-down':
+			case 'zoom-out-letter-rotate':
+			case 'zoom-out-letter-spacing': {
+				const childAnim = anime({
+					targets: target,
+					...animationPresets["in"][animationIn]["child"],
+					duration: animationInDuration,
+				});
+				const parentAnim = anime({
+					targets: item,
+					...animationPresets["in"][animationIn]["parent"],
+					duration: childAnim.duration,
+				});
+				break;
+			}
+			case 'zoom-out-elastic':
+			case 'zoom-out-offset-elastic':
+			case 'zoom-out-offset-random-elastic':
+			case 'letter-fly-up-elastic':
+			case 'letter-fly-down-elastic':
+			case 'zoom-in-elastic':
+			case 'zoom-in-offset-elastic':
+			case 'zoom-in-offset-random-elastic': {
+				anime({
+					targets: target,
+					...animationPresets["in"][animationIn.replace("-elastic", "")],
+					easing: 'spring(1, 80, 10, 0)',
+					duration: animationInDuration,
+				});
+				break;
+			}
+			default: {
+				anime({
+					targets: target,
+					...animationPresets["in"][animationIn],
+					duration: animationInDuration,
+				});
+				break;
+			}
+		}
+	}
+
+	if(animationOut) {
+		switch(animationOut) {
+			case 'zoom-out-letter-rotate':
+			case 'zoom-in-letter-fly-up':
+			case 'zoom-in-letter-fly-down':
+			case 'zoom-in-letter-rotate': {
+				const childAnim = anime({
+					targets: target,
+					...animationPresets["out"][animationOut]["child"],
+					duration: animationOutDuration,
+				});
+				const parentAnim = anime({
+					targets: item,
+					...animationPresets["out"][animationOut]["parent"],
+					duration: childAnim.duration,
+				});
+				break;
+			}
+			case 'zoom-out-elastic':
+			case 'zoom-out-offset-elastic':
+			case 'zoom-out-offset-random-elastic':
+			case 'letter-fly-up-elastic':
+			case 'letter-fly-down-elastic':
+			case 'zoom-in-elastic':
+			case 'zoom-in-offset-elastic':
+			case 'zoom-in-offset-random-elastic': {
+				anime({
+					targets: target,
+					...animationPresets["out"][animationOut.replace("-elastic", "")],
+					easing: 'easeInElastic(1, .6)',
+					duration: animationOutDuration,
+				});
+				break;
+			}
+			default: {
+				anime({
+					targets: target,
+					...animationPresets["out"][animationOut],
+					duration: animationOutDuration,
+				});
+				break;
+			}
+		}
+	}
+});
